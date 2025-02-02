0d79509932c293d67625bdb02b1886bf8e20c8cb
NaN0987 <116316543+NaN0987@users.noreply.github.com>
2025-02-01 19:26:17 -0500

    Updated reflective tape command

--- src/main/java/frc/robot/commands/vision/ReflectiveTapeExample.java
+++ src/main/java/frc/robot/commands/vision/ReflectiveTapeExample.java
@@ -1,20 +1,13 @@
 package frc.robot.commands.vision;
 
 import frc.robot.subsystems.DriveSubsystem;
-
-//Import subsystem(s) this command interacts with below
-
 import frc.robot.subsystems.VisionSubsystem;
 import edu.wpi.first.math.controller.PIDController;
-import edu.wpi.first.math.geometry.Translation2d;
-import frc.robot.Constants.FieldConstants;
 
-//Import this so you can make this class a command
 import edu.wpi.first.wpilibj2.command.Command;
 
-public class AutoAlignCircle extends Command {
+public class ReflectiveTapeExample extends Command {
 
-    //Import any instance variables that are passed into the file below here, such as the subsystem(s) your command interacts with.
     final VisionSubsystem m_visionSubsystem;
     final DriveSubsystem m_driveSubsystem;
     final PIDController distanceController = new PIDController(.7, 0, 0.1);
@@ -22,29 +15,24 @@ public class AutoAlignCircle extends Command {
     boolean isAlignDistacne = false;
     boolean isAlignRotation = false;
 
-
     final double kRotationTolerance = 2;
     final double kRotationSpeed = 0.2;
 
-
-    //If you want to contoll whether or not the command has ended, you should store it in some sort of variable:
-    private boolean m_complete = false;
-
-    //Class Constructor
-    public AutoAlignCircle(VisionSubsystem visionSubsystem, DriveSubsystem driveSubsystem){
+    /**
+     * A rough outline of a command that orbits a position marked by reflective tape.
+     * This command is no longer necessary since reflective tape isn't used anymore.
+     * @param visionSubsystem
+     * @param driveSubsystem
+     */
+    public ReflectiveTapeExample(VisionSubsystem visionSubsystem, DriveSubsystem driveSubsystem){
         m_driveSubsystem = driveSubsystem;
         m_visionSubsystem = visionSubsystem;
         
         //If your command interacts with any subsystem(s), you should pass them into "addRequirements()"
         //This function makes it so your command will only run once these subsystem(s) are free from other commands.
         //This is really important as it will stop scenarios where two commands try to controll a motor at the same time.
-        addRequirements(m_visionSubsystem);
+        addRequirements(m_visionSubsystem, m_driveSubsystem);
     }
-
-
-
-    /*Like Robot.java, there are a series of functions that you can override to give the command functionality. */
-    
 
     /*This function is called once when the command is schedueled.
      * If you are overriding "isFinished()", you should probably use this to set m_complete to false in case a command object is 
@@ -53,11 +41,7 @@ public class AutoAlignCircle extends Command {
     //When not overridden, this function is blank.
     @Override
     public void initialize(){
-        //m_chassisSubsystem.setBrakeMode();
-
         m_visionSubsystem.setPipeline(3);
-
-        m_complete = false;
     }
 
     /*This function is called repeatedly when the schedueler's "run()" function is called.
@@ -70,20 +54,16 @@ public class AutoAlignCircle extends Command {
         double targets = m_visionSubsystem.getTargets();
         double rotate = 0;
         double orbitSpeed = 0.2; // Adjust this value to control the orbit speed
-        Translation2d pos1 = m_driveSubsystem.getPose().getTranslation();
-        Translation2d pos2 = FieldConstants.kRandomPosition;
-       // Rotation2d distanceFromTarget = OdometryUtils.getDistacnePosToPos(pos1, pos2);
 
         if (targets > 0) {
             if (x > kRotationTolerance){
-                rotate = kRotationSpeed;//.6
+                rotate = kRotationSpeed;
             }
             else if (x < -kRotationTolerance){
                 rotate = -kRotationSpeed;
             }
 
-            //double forwardSpeed = distanceController.calculate(distanceFromTarget);
-            double forwardSpeed = 0; //needs to be implemengted
+            double forwardSpeed = distanceController.calculate(y, 10); //needs to be implemented
 
             // Drive the robot with orbit control
             m_driveSubsystem.drive(
@@ -97,9 +77,7 @@ public class AutoAlignCircle extends Command {
             // No targets, stop the robot
             m_driveSubsystem.drive(0, 0, 0, false, true);
         }
-    }
-
-    
+    }    
 
     /*This function is called once when the command ends.
      * A command ends either when you tell it to end with the "isFinished()" function below, or when it is interupted.
@@ -109,10 +87,5 @@ public class AutoAlignCircle extends Command {
     @Override
     public void end(boolean interrupted){
         m_driveSubsystem.drive(0, 0, 0, false, false);
-    }
-
-    @Override
-    public boolean isFinished(){
-        return m_complete;
     }
 }
